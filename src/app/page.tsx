38ff3df89c2f0429e0c4521eaf2a200666c15cbf
Mike31860 <miguel980429@gmail.com>
2023-08-31 09:13:28 -0400

    removed unused deps

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 "use client";
 
 import HomeView from "./home";
-import { useRouter } from "next/navigation";
 import { Post } from "@domain/models/post";
 import { Suspense, useEffect, useState } from "react";
 import Loading from "./loading";
-import { getPosts } from "./serverActions/posts";
 
 const basePost: Post = {
   author: {
