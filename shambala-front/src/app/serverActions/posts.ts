0b9948009a586d32ee04464165b9bf046e2b864a
Mike31860 <miguel980429@gmail.com>
2023-08-31 05:07:16 -0400

    fixed errors on like icon

--- shambala-front/src/app/serverActions/posts.ts
+++ shambala-front/src/app/serverActions/posts.ts
@@ -44,6 +44,8 @@ export const getPostDetails = async (postId: string) => {
       `${process.env.NEXT_PUBLIC_API_BASE}/api/posts/${postId}`
     );
     const rawPost = await response.json();
+
+    console.log("RAW POST ", rawPost);
     const post = mapApiPosts(rawPost);
 
     const commentsResponse = await fetch(
@@ -51,7 +53,6 @@ export const getPostDetails = async (postId: string) => {
     );
 
     const rawComments = await commentsResponse.json();
-    console.log("raw comments ", rawComments);
 
     const comments = rawComments.map(mapApiComment);
 
@@ -71,7 +72,7 @@ export const doLikePost = async (postId: string) => {
         method: "POST",
       }
     );
-    return response.json();
+    return response;
   } catch (error) {
     console.debug("FETCH ERROR ON POST LIKE ", error);
   }
