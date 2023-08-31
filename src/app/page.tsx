a79707ddb3c501bc98bb123b0b29eb5218c83a2b
Mike31860 <miguel980429@gmail.com>
2023-08-31 09:13:12 -0400

    added default posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,16 +7,32 @@ import { Suspense, useEffect, useState } from "react";
 import Loading from "./loading";
 import { getPosts } from "./serverActions/posts";
 
+const basePost: Post = {
+  author: {
+    username: "Miguel Romero",
+  },
+  description: "A normal and random post",
+  id: "1",
+  slug: {
+    current: "1",
+  },
+  createdAt: new Date(),
+  title: "A shadow under the new day",
+  image: {
+    alt: "Image",
+    src: "https://www.adorama.com/alc/wp-content/uploads/2018/11/landscape-photography-tips-yosemite-valley-feature.jpg",
+  },
+};
+
 const HomePage = () => {
   const [posts, setPosts] = useState<Post[]>();
 
   useEffect(() => {
-    const onGettingPosts = async () => {
-      const posts = await getPosts();
-      setPosts(posts);
-    };
-
-    onGettingPosts();
+    const newPosts: Post[] = [];
+    for (let i = 0; i < 10; i++) {
+      newPosts.push({ ...basePost, id: i.toString() });
+    }
+    setPosts(newPosts);
   }, []);
 
   return (
